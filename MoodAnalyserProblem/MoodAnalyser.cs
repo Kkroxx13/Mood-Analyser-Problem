25292475879459f644ae22fd9c5921dd55234fdd
Karan Kanchan <karan.kanchan@morningstar.com>
2021-08-27 21:43:18 +0530

    Added: Mood Analysis Function

--- MoodAnalyserProblem/MoodAnalyser.cs
+++ MoodAnalyserProblem/MoodAnalyser.cs
@@ -4,9 +4,21 @@ namespace MoodAnalyserProblem
 {
     class MoodAnalyser
     {
+        public string AnalyseMood(string message)
+        {
+            if (message.Contains("sad", StringComparison.OrdinalIgnoreCase))
+            {
+                return "Sad Mood";
+            }
+            else
+            {
+                return "Happy Mood";
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MoodAnalyser moodAnalyser = new MoodAnalyser();
+            string mood = moodAnalyser.AnalyseMood("I am very happy today");
         }
     }
 }
